c03579ed36e66022a4bbd572bb390f578f5a94f1
nantenainatojo <32361197+nantenainatojo@users.noreply.github.com>
2018-03-03 21:40:58 +0300

    test commit 3

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ angular.module('app', ['ngMessages'])
 		 .then(function(data){
 		 ctrl.fields=data;
 		 });
-    });
+    })
 	
 	.factory('formManaj',function($http){
 	var urlDesc = 'https://api.mongolab.com/api/1/databases/forms/collections/forms/51669d15e4b04a20de65fc58?apiKey=d3qvB8ldYFW2KSynHRediqLuBLP8JA8i';
