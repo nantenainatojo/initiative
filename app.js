a7e79a373f15b9bda0bd561c2ece775cf19b75bf
nantenainatojo <32361197+nantenainatojo@users.noreply.github.com>
2018-03-03 12:58:36 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('app', ['ngMessages'])
+    .controller('DynFormCtrl', function (formManaj) {
+var ctrl=this;
+ formManaj
+ .loadDesc()
+ .then(function(data){
+ ctrl.fields=data;
+ });
+        
+    });
+	
+	.factory('formManaj',function($http){
+	var urlDesc = 'https://api.mongolab.com/api/1/databases/forms/collections/forms/51669d15e4b04a20de65fc58?apiKey=d3qvB8ldYFW2KSynHRediqLuBLP8JA8i';
+    var urlSave = 'https://api.mongolab.com/api/1/databases/forms/collections/data?apiKey=d3qvB8ldYFW2KSynHRediqLuBLP8JA8i';
+		return {
+		loadDesc: function(){
+		return $http.get(urlDesc).then(function(response){
+			return response.data.fields;
+		}, function(response){
+			throw "Description not found";
+		}
+		);
+		}
+		}
+	});
+
